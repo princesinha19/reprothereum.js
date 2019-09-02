9d4fb6425a829ab13f6b91006f71923f60f9f3a8
princesinha19 <sinhaprince013@gmail.com>
2019-09-02 13:31:05 +0530

    changes commit

--- routes/web3js/eth-accounts.ts
+++ routes/web3js/eth-accounts.ts
@@ -24,24 +24,17 @@ const options = {
 const accounts = new Accounts('http://127.0.0.1:8545', null, options);
 
 /* POST request for creating account. */
-router.post('/create', function (req: Request, res: Response) {
+router.post('/create', async function (req: Request, res: Response) {
 
     let entropy: string = req.body.entropy;
 
-    accounts.create(entropy).then((result: any) => {
+    var result: Object = await accounts.create(entropy);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
@@ -50,20 +43,13 @@ router.post('/privateKeyToAccount', function (req: Request, res: Response) {
 
     let privateKey: string = req.body.privateKey;
 
-    accounts.privateKeyToAccount(privateKey).then((result: any) => {
+    let result: string = accounts.privateKeyToAccount(privateKey);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
@@ -107,74 +93,53 @@ router.post('/signTransaction', function (req: Request, res: Response) {
 });
 
 /* POST request for recovring transaction from raw transaction. */
-router.post('/recoverTransaction', function (req: Request, res: Response) {
+router.post('/recoverTransaction', async function (req: Request, res: Response) {
 
     let rawTransaction: string = req.body.rawTransaction;
 
-    accounts.recoverTransaction(rawTransaction).then((result: string) => {
+    let result: string = await accounts.recoverTransaction(rawTransaction);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for hashing a message hash. */
-router.post('/hashMessage', function (req: Request, res: Response) {
+router.post('/hashMessage', async function (req: Request, res: Response) {
 
     let message: string = req.body.message;
 
-    accounts.hashMessage(message).then((result: string) => {
+    let result: string = accounts.hashMessage(message)
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for signing some data. */
-router.post('/sign', function (req: Request, res: Response) {
+router.post('/sign', async function (req: Request, res: Response) {
 
     let data: string = req.body.data;
     let privateKey: string = req.body.privateKey
 
-    accounts.sign(data, privateKey).then((result: any) => {
+    let result: object = await accounts.sign(data, privateKey)
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for recovering the address. */
-router.post('/recover', function (req: Request, res: Response) {
+router.post('/recover', async function (req: Request, res: Response) {
 
     let messageHash: string = req.body.messageHash;
     let r: string = req.body.r;
@@ -182,304 +147,206 @@ router.post('/recover', function (req: Request, res: Response) {
     let v: string = req.body.v;
     let preFixed: boolean = req.body.preFixed;
 
-    accounts.sign(messageHash, v, r, s, preFixed).then((result: string) => {
+    let result: string = await accounts.sign(messageHash, v, r, s, preFixed);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request to encrypt the account in keystore format from private key. */
-router.post('/encrypt', function (req: Request, res: Response) {
+router.post('/encrypt', async function (req: Request, res: Response) {
 
     let privateKey: string = req.body.privateKey;
     let password: string = req.body.password;
 
-    accounts.sign(privateKey, password).then((result: any) => {
+    let result: object = await accounts.sign(privateKey, password);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request to decrypt the account using keystore file. */
-router.post('/decrypt', function (req: Request, res: Response) {
+router.post('/decrypt', async function (req: Request, res: Response) {
 
     let keystoreJsonV3: any = req.body.keystoreJsonV3;
     let password: string = req.body.password;
 
-    accounts.sign(keystoreJsonV3, password).then((result: any) => {
+    let result: object = await accounts.sign(keystoreJsonV3, password);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* GET request to get all in memory wallets. */
-router.get('/wallet', function (req: Request, res: Response) {
+router.get('/wallet', async function (req: Request, res: Response) {
 
-    accounts.wallet().then((result: any) => {
+    let result: any = await accounts.wallet();
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for creating a wallet. */
-router.post('/wallet/create', function (req: Request, res: Response) {
+router.post('/wallet/create', async function (req: Request, res: Response) {
 
     let numberOfAccounts: number = req.body.numberOfAccounts;
     let entropy: string = req.body.entropy;
 
-    accounts.wallet.create(numberOfAccounts, entropy).then((result: any) => {
+    let result: any = await accounts.wallet.create(numberOfAccounts, entropy);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for adding a wallet using private key. */
-router.post('/wallet/add', function (req: Request, res: Response) {
+router.post('/wallet/add', async function (req: Request, res: Response) {
 
     let privateKey: string = req.body.privateKey;
     let stringObject: any = req.body.stringObject;
 
     if (req.body.privateKey){
-        accounts.wallet.add(privateKey).then((result: any) => {
+        let result: any = await accounts.wallet.add(privateKey);
 
-            console.log(result);
+        console.log(result);
 
-            res.status(200).json({
-                success: true,
-                result: result
-            });
-        })
-        .catch((error: Error) => {
-            res.status(500).json({
-                success: false,
-                result: error.message
-            });
+        res.status(200).json({
+            success: true,
+            result: result
         });
     }
     else{
-        accounts.wallet.add(stringObject).then((result: any) => {
+        let result: any = await accounts.wallet.add(stringObject);
 
-            console.log(result);
+        console.log(result);
 
-            res.status(200).json({
-                success: true,
-                result: result
-            });
-        })
-        .catch((error: Error) => {
-            res.status(500).json({
-                success: false,
-                result: error.message
-            });
+        res.status(200).json({
+            success: true,
+            result: result
         });
     }
 });
 
 /* POST request for removing a wallet. */
-router.post('/wallet/remove', function (req: Request, res: Response) {
+router.post('/wallet/remove', async function (req: Request, res: Response) {
 
     let address: string = req.body.address;
     let index: any = req.body.index;
 
     if (req.body.address){
-        accounts.wallet.remove(address).then((result: boolean) => {
+        let result: any = await accounts.wallet.remove(address);
 
-            console.log(result);
+        console.log(result);
 
-            res.status(200).json({
-                success: true,
-                result: result
-            });
-        })
-        .catch((error: Error) => {
-            res.status(500).json({
-                success: false,
-                result: error.message
-            });
+        res.status(200).json({
+            success: true,
+            result: result
         });
     }
     else{
-        accounts.wallet.remove(index).then((result: boolean) => {
+        let result: any = await accounts.wallet.remove(index);
 
-            console.log(result);
+        console.log(result);
 
-            res.status(200).json({
-                success: true,
-                result: result
-            });
-        })
-        .catch((error: Error) => {
-            res.status(500).json({
-                success: false,
-                result: error.message
-            });
+        res.status(200).json({
+            success: true,
+            result: result
         });
     }
 });
 
 /* GET request for clearing wallet. */
-router.get('/wallet/clear', function (req: Request, res: Response) {
+router.get('/wallet/clear', async function (req: Request, res: Response) {
 
-    accounts.wallet.clear().then((result: any) => {
+    let result: object[] = await accounts.wallet.clear();
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for encrypting all wallet, which is inside keystore file. */
-router.post('/wallet/encrypt', function (req: Request, res: Response) {
+router.post('/wallet/encrypt', async function (req: Request, res: Response) {
 
     let password: string = req.body.password;
 
-    accounts.wallet.encrypt(password).then((result: any) => {
+    let result: any = await accounts.wallet.encrypt(password);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for decrypting a wallet, which is inside keystore file. */
-router.post('/wallet/decrypt', function (req: Request, res: Response) {
+router.post('/wallet/decrypt', async function (req: Request, res: Response) {
 
     let keystoreArray: [] = req.body.keystoreArray;
     let password: string = req.body.password;
 
-    accounts.wallet.decrypt(keystoreArray, password).then((result: any) => {
+    let result: object = await accounts.wallet.decrypt(keystoreArray, password);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for storing a wallet in local storage. */
-router.post('/wallet/save', function (req: Request, res: Response) {
+router.post('/wallet/save', async function (req: Request, res: Response) {
 
     let password: string = req.body.password;
     let keyName: string = req.body.string;
 
-    accounts.wallet.save(password, keyName).then((result: boolean) => {
+    let result: boolean = await accounts.wallet.save(password, keyName);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
 /* POST request for loading a wallet from local storage. */
-router.post('/wallet/load', function (req: Request, res: Response) {
+router.post('/wallet/load', async function (req: Request, res: Response) {
 
     let password: string = req.body.password;
     let keyName: string = req.body.string;
 
-    accounts.wallet.load(password, keyName).then((result: any) => {
+    let result: any = await accounts.wallet.load(password, keyName);
 
-        console.log(result);
+    console.log(result);
 
-        res.status(200).json({
-            success: true,
-            result: result
-        });
-    })
-    .catch((error: Error) => {
-        res.status(500).json({
-            success: false,
-            result: error.message
-        });
+    res.status(200).json({
+        success: true,
+        result: result
     });
 });
 
